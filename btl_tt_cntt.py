f6833b9df767fb9ad7359f695bb5247bdde6b822
mjnk9xw <minhnguyen998vp@gmail.com>
2020-05-26 01:32:28 +0700

    ok

--- btl_tt_cntt.py
+++ btl_tt_cntt.py
@@ -6,7 +6,7 @@ class Program:
     countPoint = x1 = x2 = y1 = y2 = 0
     arrResult = []
     def inputAll(self):
-        print('nhap so luong diem: ')
+        print('nhập số lượng điểm: ')
         self.countPoint = int(input())
         print('nhập lần lượt 2 điểm bất kì để chọn đường đi ngắn nhất: ')
         print('tọa độ điểm thứ nhất: ')
@@ -15,6 +15,7 @@ class Program:
         print('tọa độ điểm thứ hai: ')
         self.x2 = int(input())
         self.y2 = int(input())
+        self.rand()
 
     def viewNetworkx(self,all):
         import networkx as nx
@@ -26,45 +27,50 @@ class Program:
         val_map = {}
         red_edges = []
         if all == True:
-            for i in range(len(self.arrResult)-1):
-                    if self.graph[self.arrResult[i],self.arrResult[i+1]] > 0:
-                        G.add_edges_from([(self.arrResult[i], self.arrResult[i+1])], weight=int(self.graph[self.arrResult[i],self.arrResult[i+1]]))
-                        val_map[self.arrResult[i]] = 1.0
-                        red_edges.append((self.arrResult[i],self.arrResult[i+1]))
-            val_map[self.arrResult[len(self.arrResult)-1]] = 1.0
-            
+            val_map[0] = 1.0
             for i in range(self.countPoint):
                 for j in range(self.countPoint):
                     if self.graph[i,j] > 0 and self.listWay[i][j] == 1:
                         G.add_edges_from([(i, j)], weight=int(self.graph[i,j]))
 
+            for i in range(len(self.arrResult)-1):
+                    u = self.arrResult[i]
+                    v = self.arrResult[i+1]
+                    print(u,v)
+                    if self.graph[u,v] > 0 and self.listWay[u,v] == 1:
+                        val_map[u] = 1.0
+                        red_edges.append((u,v))
+            val_map[self.arrResult[len(self.arrResult)-1]] = 1.0
+            
         else:
             for i in range(self.countPoint):
                 for j in range(self.countPoint):
-                    if self.graph[i,j] > 0:
+                    if self.graph[i,j] > 0 and self.listWay[i,j] == 1:
                         G.add_edges_from([(i, j)], weight=int(self.graph[i,j]))
-
         
+        print(red_edges)
+        print(val_map)
+
         values = [val_map.get(node, 0.45) for node in G.nodes()]
         edge_labels=dict([((u,v,),d['weight'])
-                        for u,v,d in G.edges(data=True)])
-      
-        edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
+                        for u,v,d in sorted(G.edges(data=True),key=lambda x: x[2]['weight'])])
+
+        edge_colors = ['grey' if not edge in red_edges else 'red' for edge in G.edges()]
         
         pos=nx.spring_layout(G)
         nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
         nx.draw(G,pos, node_color = values, node_size=500,edge_color=edge_colors,edge_cmap=plt.cm.Reds,with_labels=True)
         pylab.show()
 
-    def viewPlot(self):
-        pl.scatter(self.randData[:,0],self.randData[:,1],50,'blue','s')
+    # def viewPlot(self):
+    #     pl.scatter(self.randData[:,0],self.randData[:,1],50,'blue','s')
       
-        if len(self.arrResult) > 0:
-            pl.plot(self.randData[self.arrResult[:],0],self.randData[self.arrResult[:],1],'go-')
+    #     if len(self.arrResult) > 0:
+    #         pl.plot(self.randData[self.arrResult[:],0],self.randData[self.arrResult[:],1],'go-')
 
-        pl.scatter(self.x1,self.y1, c='r')
-        pl.scatter(self.x2,self.y2, c='r')
-        pl.show()
+    #     pl.scatter(self.x1,self.y1, c='r')
+    #     pl.scatter(self.x2,self.y2, c='r')
+    #     pl.show()
 
     def rand(self):
         self.randData = np.random.randint(-100,100,(self.countPoint,2)) 
@@ -121,7 +127,8 @@ class Program:
                 else:
                     begin = self.randData[i]
                     end = self.randData[j]
-                    self.graph[i][j] = np.sqrt(np.power(end[0] - begin[0],2) + np.power(end[1] - begin[1],2))
+                    self.graph[i][j] = int(np.sqrt(np.power(end[0] - begin[0],2) + np.power(end[1] - begin[1],2)))
+        print(self.graph)
 
     def dijkstra(self,src, endpoint):
         print('processing ...............')
@@ -133,40 +140,47 @@ class Program:
    
         for cout in range(self.countPoint): 
             u = self.minDistance(dist,cout, checkVisit) 
-            print(u)
+            # print(u)
             if u >= 0:
                 checkVisit[u] = True
-                self.arrResult.append(u)
-                if u == endpoint:
-                    print(self.arrResult)
-                    # return 
+                # self.arrResult.append(u)
+                # if u == endpoint:
+                #     print(self.arrResult)
+                # return 
                 for v in range(self.countPoint): 
-                    
                     if self.graph[u][v] > 0 and checkVisit[v] == False and dist[v] > dist[u] + self.graph[u][v] and self.listWay[u][v] == 1: 
                         dist[v] = dist[u] + self.graph[u][v]
                         parent[v] = u
 
         self.printSolution(dist,parent)
 
-    def printPath(self, parent, j): 
-          
-        #Base Case : If j is source 
+    def printPath(self, parent, j, appendOk): 
         if parent[j] == -1 :  
             print(j)
             return
-        self.printPath(parent , parent[j]) 
+        self.printPath(parent , parent[j],appendOk) 
         print(j)
+        if appendOk == True:
+            self.arrResult.append(j)
         
     def printSolution(self, dist, parent): 
         src = 0
+        self.arrResult.append(src)
         print("Vertex \t\tDistance from Source\tPath") 
         for i in range(1, len(dist)): 
-            print("\n%d --> %d \t\t%d \t\t\t\t\t" % (src, i, dist[i])), 
-            self.printPath(parent,i) 
+            print("\n%d --> %d \t\t%d \t\t" % (src, i, dist[i])), 
+            if i == self.countPoint - 1:
+                self.printPath(parent,i, True)
+            else:
+                self.printPath(parent,i, False)
+        
+        if len(self.arrResult) == 2 and dist[self.arrResult[1]] == 100000:
+            print("Không thể tìm được đường đi giữa điểm 2 điểm: 0 và ",self.arrResult[1])
+        else:
+            print("Đường đi giữ 2 điểm: ",self.arrResult.__str__())
 
 main = Program()
 main.inputAll()
-main.rand()
 main.viewNetworkx(False)
 main.process()
 main.viewNetworkx(True)
